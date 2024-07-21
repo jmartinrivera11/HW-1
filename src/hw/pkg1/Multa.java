58d13ff52371a8ddaf946f9cfec08b4f8677e083
Jose Rivera <josemartinriverasolis@gmail.com>
2024-07-21 11:28:01 -0600

    first commit

---
+++ src/hw/pkg1/Multa.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hw.pkg1;
+
+public class Multa {
+    
+    private int codigo;
+    private String nombre;
+    private String tipo;
+    private double monto;
+    private boolean pagado;
+    
+    public double TIPO_GRAVE;
+    public double TIPO_MEDIO;
+    public double TIPO_LEVE;
+    
+    public Multa(int codigo, String nombre, String tipo) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        pagado = false;
+        
+        TIPO_GRAVE = 250;
+        TIPO_MEDIO = 125;
+        TIPO_LEVE = 50;
+        
+        switch (tipo) {
+            case "LEVE" -> monto = TIPO_LEVE;
+            case "MEDIO" -> monto = TIPO_MEDIO;
+            case "GRAVE" -> monto = TIPO_GRAVE;
+            default -> monto = TIPO_LEVE;
+        }
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public String getTipo() {
+        return tipo;
+    }
+    
+    public int getCodigo() {
+        return codigo;
+    }
+    
+    public double getMonto() {
+        return monto;
+    }
+    
+    public boolean getPagado() {
+        return pagado;
+    }
+    
+    public String pay() {
+        pagado = true;
+        return "Multa con Código #" + codigo + " fue "
+                + "pagada por un monto de Lps." + monto;
+    }
+    
+    public String print() {
+        String text = (pagado == true) ? "Pagada por un monto de Lps." 
+                + monto : "Multa pendiente";
+        return text;
+    }
+}
---
+++ src/hw/pkg1/Transito.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hw.pkg1;
+
+public class Transito {
+    
+    Multa[] multas;
+    int contador;
+    
+    public Transito() {
+        multas = new Multa[100];
+        contador = 0;
+    }
+    
+    public Multa buscar(int codigo) {
+        for (Multa multa : multas) {
+            if (multa != null && multa.getCodigo() == codigo) {
+                return multa;
+            }
+        }
+        return null;
+    }
+    
+    public boolean agregarMulta(int codigo, String nombre, String tipo) {
+        if (buscar(codigo) == null)
+            for (int i = 0; i < multas.length; i++) {
+                if (multas[i] == null) {
+                    multas[i] = new Multa(codigo, nombre, tipo);
+                    contador += 1;
+                    return true;
+                }
+            }
+        return false;
+    }
+    
+    public boolean pagarMulta(int codigo) {
+        Multa multa = buscar(codigo);
+        if (multa != null) {
+            multa.pay();
+            return true;
+        }
+        return false;
+    }
+    
+    public String print() {
+        String text = "";
+        for (int i = 0; i < multas.length; i++)
+            if (multas[i] != null)
+                text = "Nombre: " + multas[i].getNombre() + "\nCódigo: " 
+                        + multas[i].getCodigo() + "\nTipo: " + multas[i].getTipo() 
+                        + "\nPagada: " + multas[i].getPagado();
+        return text;
+    }
+    
+    public String info() {
+        String text;
+        int pagada = 0, pendiente = 0;
+        
+        for (int i = 0; i < multas.length; i++) {
+            if (multas[i] != null) {
+                if (multas[i].getPagado())
+                    pagada += 1;
+                pendiente += 1;
+            }
+        }       
+        
+        text = "Cantidad de multas generadas: " + contador + "\nCantidad de "
+                + "Multas Pagadas: " + pagada + "\nCantidad de Multas "
+                + "Pendientes: " + pendiente;
+        return text;
+    }
+}
